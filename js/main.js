1b8e3ea8935bb868bb8c3b145bd3c6af19283053
carlo briganti <505641nl@saeinstitute.edu>
2020-07-06 15:35:21 +0200

    inserito cotrolli per vedere se cio che hai scritto e maggiore di tre caratteri se sono lettere e quello di base per evitare che sia una stringa vuota

--- js/main.js
+++ js/main.js
@@ -6,13 +6,29 @@ var colore = prompt('il tuo colore prefito e?')
 
 var numero = Math.floor(Math.random() * 323818320) + 9203213921;  
 
+var tooshort = true;
+
+var noLetters = false;
+
+var letters = /^[A-Za-z]+$/;
+
 document.getElementById("titolo").innerHTML = "Password generator"
 
 var password = nome + cognome + colore + numero;
-if(nome != "" && cognome != "" && colore != ""){
+
+// controllo per vedere se cio che hai inserito e piu lungo di tre caratteri
+if(nome.length < 3 || cognome.length  < 3 || colore.length < 3){
+    tooshort  = false;
+}
+// controllo per vedere se quello che hai inserito sono delle lettere
+if (nome.match(letters)){
+    noLetters = true;
+ }
+
+if(nome != "" && cognome != "" && colore != "" && tooshort != false && noLetters != false){
     document.getElementById("password").innerHTML = "La tua password a prova di bomba e: "+ password;
 }else{
-  alert("Iserisci un valore nei campi");
+  alert("I valori inseriti non sono corretti");
   location.reload();
 }
-// chiedi all'itente nome cognome colore preferito e un numero;
+
